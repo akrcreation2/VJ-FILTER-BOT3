be11aa030d2d65f15b01b113e361386667deb87c
akrcreation2 <88943345+akrcreation2@users.noreply.github.com>
2025-03-02 17:26:42 +0530

    Update __init__.py

--- web/__init__.py
+++ web/__init__.py
@@ -1,15 +1,7 @@
 # Credit - adarsh-goel
 
 from aiohttp import web
-from web.stream_routes import routes  # Assuming routes is defined in stream_routes.py
+from web.stream_routes import routes
 
-# Create a new aiohttp Application
 web_app = web.Application()
-
-# Add routes to the application
 web_app.add_routes(routes)
-
-# Define the main entry point for the app
-if __name__ == '__main__':
-    # Run the application on a specific host and port (for example, localhost:8080)
-    web.run_app(web_app, host='0.0.0.0', port=8080)
