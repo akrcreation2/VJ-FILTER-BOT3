accc9be950e163ac3213cccda53d8143116f5e5a
akrcreation2 <88943345+akrcreation2@users.noreply.github.com>
2025-03-06 20:33:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -122,7 +122,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://knmlpro2filter-akrcreation2.koyeb.app/")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
